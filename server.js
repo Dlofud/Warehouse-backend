ab4e024ff90692408b604677a3e010c0f185fa7f
Dlofud <4driano.meis@gmail.com>
2025-08-11 13:01:43 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Warehouse backend is working!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
